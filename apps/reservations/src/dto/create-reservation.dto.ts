12a63ece9c8a2ae248fcc737b2c05e776e98e053
Daniel <leinadpy@gmail.com>
2023-07-23 16:42:54 -0400

    Mundando a ubuntu

---
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -0,0 +1,17 @@
+import { IsDate, IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateReservationDto {
+  @IsDate()
+  startDate: Date;
+
+  @IsDate()
+  endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
+}
---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,17 @@
+import { AbstracRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ReservationDocument } from './models/reservation.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ReservationsRepository extends AbstracRepository<ReservationDocument> {
+  protected readonly logger = new Logger(ReservationsRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
+}
